a32260d4e8df193ab9b5439e847b9b7acec47f7c
Alex Lynham <alex@lynh.am>
2020-02-15 11:51:42 +0000

    First commit - WIP on instrux, example app building okay

---
+++ project.clj
@@ -0,0 +1,49 @@
+(defproject todomvc "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library
+                               org.clojure/google-closure-library-third-party]]
+                 [thheller/shadow-cljs "2.8.83"]
+                 [reagent "0.9.1"]
+                 [re-frame "0.11.0"]
+                 [clj-commons/secretary "1.2.4"]]
+
+  :plugins [
+            [lein-shell "0.5.0"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :test-paths   ["test/cljs"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
+                                    "test/js"]
+
+
+  :shell {:commands {"open" {:windows ["cmd" "/c" "start"]
+                             :macosx  "open"
+                             :linux   "xdg-open"}}}
+
+  :aliases {"dev"          ["with-profile" "dev" "do"
+                            ["run" "-m" "shadow.cljs.devtools.cli" "watch" "app"]]
+            "prod"         ["with-profile" "prod" "do"
+                            ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
+            "build-report" ["with-profile" "prod" "do"
+                            ["run" "-m" "shadow.cljs.devtools.cli" "run" "shadow.cljs.build-report" "app" "target/build-report.html"]
+                            ["shell" "open" "target/build-report.html"]]
+            "karma"        ["with-profile" "prod" "do"
+                            ["run" "-m" "shadow.cljs.devtools.cli" "compile" "karma-test"]
+                            ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]}
+
+  :profiles
+  {:dev
+   {:dependencies [[binaryage/devtools "1.0.0"]
+                   [day8.re-frame/re-frame-10x "0.5.1"]
+                   [day8.re-frame/tracing "0.5.3"]]
+    :source-paths ["dev"]}
+
+   :prod { :dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]}}
+
+  :prep-tasks [])
